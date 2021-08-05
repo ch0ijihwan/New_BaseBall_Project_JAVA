8fa27c698a27db3fd6510647b6a8bacefce8fc59
cjh8746 <cjh87467@gmail.com>
2021-08-05 13:18:50 +0900

    refactor : 줄 띄움 부분 글자 정렬 깨짐 오류 수정

--- src/main/java/view/StatusDisplay.java
+++ src/main/java/view/StatusDisplay.java
@@ -1,25 +1,23 @@
 package view;
 
-import static org.graalvm.compiler.hotspot.replacements.Log.println;
-
 public class StatusDisplay {
     private StatusDisplay() {
 
     }
 
     public static void showBallAndStrike(int ball, int strike) {
-        println("Ball : " + ball + "Strike : " + strike);
+        System.out.println("Ball : " + ball + "   Strike : " + strike);
     }
 
     public static void showStartGame() {
-        println("숫자 야구 게임을 시작 합니다.");
+        System.out.println("숫자 야구 게임을 시작 합니다.");
     }
 
     public static void showEndGame() {
-        println("게임을 끝 마칩니다.");
+        System.out.println("게임을 끝 마칩니다.");
     }
 
     public static void showNewGameQuestion() {
-        println("게임을 재실행 하려면 1을 누르세요 \n 게임을 종료하시려면 2를 누르세요");
+        System.out.println("게임을 재실행 하려면 1을 누르세요 \n게임을 종료하시려면 2를 누르세요");
     }
 }
