c86222973b3fca5c8c18aef2a73e2c771c9bc120
cjh8746 <cjh87467@gmail.com>
2021-08-05 12:48:36 +0900

    feat : 숫자야구 게임의 한 라운드를 실행시키는 컨트롤러 구현

---
+++ src/main/java/controller/BaseballController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.EnterNumbers;
+import model.RandomBaseballNumbers;
+
+import static model.score.Ball.countBall;
+import static model.score.Strike.countStrike;
+import static view.Input.inputBaseballNumbers;
+import static view.StatusDisplay.showBallAndStrike;
+import static view.StatusDisplay.showEndGame;
+
+public class BaseballController {
+    private EnterNumbers enteredNumbers;
+    private final RandomBaseballNumbers randomBaseballNumbers;
+
+    BaseballController() {
+        enteredNumbers = new EnterNumbers(inputBaseballNumbers());
+        randomBaseballNumbers = new RandomBaseballNumbers();
+    }
+
+    void playGame() {
+        while (isThreeStrike()) {
+            showBallAndStrike(getBallCount(), getStrikeCount());
+            enterNumbers();
+        }
+        showEndGame();
+    }
+
+    void enterNumbers() {
+        enteredNumbers = new EnterNumbers(inputBaseballNumbers());
+    }
+
+    int getBallCount() {
+        return countBall(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers());
+    }
+
+    int getStrikeCount() {
+        return countStrike(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers());
+    }
+
+    boolean isThreeStrike() {
+        return countStrike(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers()) != 3;
+    }
+}
