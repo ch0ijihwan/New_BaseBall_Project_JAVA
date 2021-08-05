b98e55934eb74f5062714276bbd86c5ac96e6448
cjh8746 <cjh87467@gmail.com>
2021-08-05 15:01:08 +0900

    refactor : 사용하지 않은 필드 제거

--- src/main/java/model/RandomBaseballNumbers.java
+++ src/main/java/model/RandomBaseballNumbers.java
@@ -3,31 +3,18 @@ package model;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import static model.score.Ball.countBall;
-import static model.score.Strike.countStrike;
 import static util.RandomUitl.generateRandomNumber;
 
 public class RandomBaseballNumbers {
     private static final int RANDOM_NUMBER_SIZE = 3;
     private final Set<Number> threeNumbers = new LinkedHashSet<>();
-    private final Set<Number> enterNumbers;
-
-    public RandomBaseballNumbers(Set<Number> enteredNumbers) {
+    public RandomBaseballNumbers(){
         while (threeNumbers.size() < RANDOM_NUMBER_SIZE) {
             threeNumbers.add(new Number(String.valueOf(generateRandomNumber())));
         }
-        this.enterNumbers = enteredNumbers;
     }
 
     public Set<Number> getRandomBaseballNumbers() {
         return this.threeNumbers;
-    }
-
-    public int getBallCount() {
-        return countBall(this.enterNumbers, this.getRandomBaseballNumbers());
-    }
-
-    public int getStrikeCount() {
-        return countStrike(this.enterNumbers, this.getRandomBaseballNumbers());
     }
 }
