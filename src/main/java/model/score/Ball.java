9dfe58a5183f60786b1653c8deb4bfc43b95bfe9
cjh8746 <cjh87467@gmail.com>
2021-08-05 12:11:57 +0900

    feat : Ball의 개수는 count 해주는 기능 구현

---
+++ src/main/java/model/score/Ball.java
@@ -0,0 +1,20 @@
+package model.score;
+
+import model.Number;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class Ball {
+    private Ball(){
+
+    }
+    public static int countBall(Set<Number> inputNumbers, Set<Number> randomNumbers) {
+
+        return (int) (randomNumbers.stream()
+                .filter(randomNumber -> inputNumbers.stream().anyMatch(Predicate.isEqual(randomNumber)))
+                .count()
+                - Strike.countStrike(inputNumbers, randomNumbers));
+    }
+}
+
