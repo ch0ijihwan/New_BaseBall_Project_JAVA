c99cd50dfd2b016919091f69b6674a3d56ea928d
cjh8746 <cjh87467@gmail.com>
2021-08-05 12:09:19 +0900

    feat : 램덤 수를 생성해주는 기능 구현

---
+++ src/main/java/model/RandomBaseballNumbers.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static util.RandomUitl.generateRandomNumber;
+
+public class RandomBaseballNumbers {
+    private static final int RANDOM_NUMBER_SIZE = 3;
+    private final Set<Number> threeNumbers = new LinkedHashSet<>();
+
+    public RandomBaseballNumbers() {
+        while (threeNumbers.size() < RANDOM_NUMBER_SIZE) {
+            threeNumbers.add(new Number(String.valueOf(generateRandomNumber())));
+        }
+    }
+
+    public Set<Number> getRandomBaseballNumbers() {
+        return this.threeNumbers;
+    }
+}
