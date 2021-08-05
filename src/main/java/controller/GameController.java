e8b89806ca6e36e01e29c2568798d82c2cef3a20
cjh8746 <cjh87467@gmail.com>
2021-08-05 13:17:23 +0900

    refactor : BaseBallController 생성 후 게임 실행 하도록 변경

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -3,18 +3,17 @@ package controller;
 import view.StatusDisplay;
 
 import static view.Input.askReplayGame;
-import static view.StatusDisplay.showEndGame;
 import static view.StatusDisplay.showNewGameQuestion;
 
 public class GameController {
     private BaseballController baseBallController;
     private boolean isGamePlay = true;
 
-    void run (){
+    public void run(){
         while (isGamePlay) {
             StatusDisplay.showStartGame();
+            baseBallController = new BaseballController();
             baseBallController.playGame();
-            showEndGame();
             showNewGameQuestion();
             isGamePlay = askReplayGame();
         }
