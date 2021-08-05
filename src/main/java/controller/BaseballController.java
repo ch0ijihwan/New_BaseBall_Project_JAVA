577a883f9e23571323f96d44079d523ebd67d7be
cjh8746 <cjh87467@gmail.com>
2021-08-05 15:00:29 +0900

    refactor : getBallCount 메소드 추가

--- src/main/java/controller/BaseballController.java
+++ src/main/java/controller/BaseballController.java
@@ -3,6 +3,7 @@ package controller;
 import model.EnterNumbers;
 import model.RandomBaseballNumbers;
 
+import static model.score.Ball.countBall;
 import static model.score.Strike.countStrike;
 import static view.Input.inputBaseballNumbers;
 import static view.StatusDisplay.showBallAndStrike;
@@ -10,26 +11,36 @@ import static view.StatusDisplay.showEndGame;
 
 public class BaseballController {
     private EnterNumbers enteredNumbers;
-    private final RandomBaseballNumbers randomBaseballNumbers;
+    private RandomBaseballNumbers randomBaseballNumbers;
 
     BaseballController() {
         enteredNumbers = new EnterNumbers(inputBaseballNumbers());
-        randomBaseballNumbers = new RandomBaseballNumbers(enteredNumbers.getEnteredNumbers());
+        randomBaseballNumbers = new RandomBaseballNumbers();
     }
 
     void playGame() {
         while (isThreeStrike()) {
-            showBallAndStrike(randomBaseballNumbers.getBallCount(),randomBaseballNumbers.getStrikeCount());
+            showBallAndStrike(getBallCount(),getStrikeCount());
             enterNumbers();
         }
         showEndGame();
     }
 
-    void enterNumbers() {
+    void enterNumbers(){
         enteredNumbers = new EnterNumbers(inputBaseballNumbers());
+    }
+
+    int getBallCount() {
+        return countBall(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers());
+    }
+
+    int getStrikeCount() {
+        return countStrike(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers());
     }
 
     boolean isThreeStrike() {
         return countStrike(enteredNumbers.getEnteredNumbers(), randomBaseballNumbers.getRandomBaseballNumbers()) != 3;
     }
+
+
 }
