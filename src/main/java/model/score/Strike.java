dd8105627586005318ad3ab2bb11e2c1fe62280b
cjh8746 <cjh87467@gmail.com>
2021-08-05 12:12:26 +0900

    feat : strike의 개수를 반환 해주는 기능 구현

---
+++ src/main/java/model/score/Strike.java
@@ -0,0 +1,19 @@
+package model.score;
+
+import model.Number;
+
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class Strike{
+    private Strike(){
+
+    }
+    private static final int NUMBER_LENGTH = 3;
+    private static final int START = 0;
+
+    public static int countStrike(Set<Number> inputNumbers, Set<Number> randomNumbers) {
+        return (int) IntStream.range(START, NUMBER_LENGTH)
+                .filter(index -> inputNumbers.toArray()[index].equals(randomNumbers.toArray()[index])).count();
+    }
+}
