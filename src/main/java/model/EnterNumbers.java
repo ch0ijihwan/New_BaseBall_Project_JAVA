44444a330d947bab5d3b985fddf2f1defa2e9e06
cjh8746 <cjh87467@gmail.com>
2021-08-05 12:43:32 +0900

    refactor : 수를 입력 받는 클래스명 변경

---
+++ src/main/java/model/EnterNumbers.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EnterNumbers {
+    private final Set<Number> values;
+
+    public EnterNumbers(String[] tokens) {
+        this.values = initValue(tokens);
+    }
+
+    private Set<Number> initValue(String[] tokens) {
+        checkEnteredNumber(tokens);
+        return Arrays.stream(tokens)
+                .map(Number::new)
+                .collect(Collectors.toSet());
+    }
+
+    private void checkEnteredNumber(String[] token) {
+        if (token.length == 3) {
+            throw new IllegalArgumentException("입력값이 3자리가 아닙니다.");
+        }
+    }
+
+    public Set<Number> getEnteredNumbers() {
+        return this.values;
+    }
+}
