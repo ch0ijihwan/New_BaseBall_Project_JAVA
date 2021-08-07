8cc5c342edfeedfe137cffc8bcf85fb646312cf8
cjh8746 <cjh87467@gmail.com>
2021-08-07 15:19:58 +0900

    test : 랜덤 베이스볼의 갯수가 세개가  각각 0보다 크고 10 보다 작은지 확인하는 테스트 코드 구현

--- src/test/java/model/RandomBaseballNumbersTest.java
+++ src/test/java/model/RandomBaseballNumbersTest.java
@@ -2,15 +2,44 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class RandomBaseballNumbersTest {
-    RandomBaseballNumbers randomBaseballNumbers = new RandomBaseballNumbers();
+
+    private static RandomBaseballNumbers randomBaseballNumbers = new RandomBaseballNumbers();
 
     @Test
     @DisplayName("램던 베이스볼의 갯수가 3개인지 확인")
     void randomBaseball() {
         assertThat(randomBaseballNumbers.getRandomBaseballNumbers().size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @MethodSource("randomBaseballParameterProvider")
+    @DisplayName("생성된 랜덤 베이스볼에 각각 0보다 크고 10 보다 작은 값이 있는지 확인")
+    void allNumberIsGreaterThanZeroAndLessThanTen(List<Number> numbers) {
+        assertAll(
+                () -> assertThat(numbers.get(0).value()).isGreaterThan(0).isLessThan(10),
+                () -> assertThat(numbers.get(1).value()).isGreaterThan(0).isLessThan(10),
+                () -> assertThat(numbers.get(2).value()).isGreaterThan(0).isLessThan(10)
+        );
+    }
+
+    static Stream<Arguments> randomBaseballParameterProvider() {
+        return Stream.of(arguments(new ArrayList<>( new RandomBaseballNumbers().getRandomBaseballNumbers())),
+                arguments(new ArrayList<>(new RandomBaseballNumbers().getRandomBaseballNumbers())),
+                arguments(new ArrayList<>(new RandomBaseballNumbers().getRandomBaseballNumbers())),
+                arguments(new ArrayList<>(new RandomBaseballNumbers().getRandomBaseballNumbers())),
+                arguments(new ArrayList<>(new RandomBaseballNumbers().getRandomBaseballNumbers())));
     }
 }
