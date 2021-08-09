b56062c7ea4ec872e5f015df97f0202f60d4291d
cjh8746 <cjh87467@gmail.com>
2021-08-09 23:57:42 +0900

    test : 랜덤 유틸의 테스트 수정(한번에 여러개의 램덤 수를 뽑아 테스트 할 수 있도록 변경)

---
+++ src/test/java/util/RandomUtilTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static util.RandomUtil.generateRandomNumber;
+
+class RandomUtilTest {
+    @ParameterizedTest
+    @MethodSource("randomUtilParameterProvider")
+    @DisplayName("0보다크고 10보다 작은 한자리수 반환")
+    void greaterThanZeroAndLessThanTen(int number) {
+        assertThat(number).isGreaterThan(0).isLessThan(10);
+    }
+
+    static Stream<Arguments> randomUtilParameterProvider() {
+        return Stream.of(arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()),
+                arguments(generateRandomNumber()));
+    }
+}
