8b2268c96ff61f24fb64bdd39a9479b888c3fa5b
cjh8746 <cjh87467@gmail.com>
2021-08-10 17:32:37 +0900

    feat : 한 라운드를 실행하는 round 클래스 구현

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -1,18 +1,62 @@
 package model;
 
+import java.util.Set;
+
+import static controller.BaseballController.resultBallCountAndStrikeCount;
+import static model.score.Ball.countBall;
+import static model.score.Strike.countStrike;
+import static view.Input.inputBaseballNumbers;
+
 public class Round {
 
-    private int roundCount = 0;
+    private int roundCount = 1;
+    private Set<Number> uesrNumbers;
+    private final Set<Number> systemNumbers;
 
-    public void upRoundCount() {
+    public Round(Set<Number> uesrNumbers, Set<Number> systemNumbers) {
+        this.uesrNumbers = uesrNumbers;
+        this.systemNumbers = systemNumbers;
+    }
+
+    public void playRound() {
+        upRoundCount();
+        resultBallCountAndStrikeCount(resultBall(), resultStrike());
+        if (isThreeStrike()) {
+            isOver();
+            resetRoundCount();
+        }
+        this.uesrNumbers = enterNumbers();
+    }
+
+    private Set<Number> enterNumbers() {
+        return new EnterNumbers(inputBaseballNumbers()).getEnteredNumbers();
+    }
+
+    private void upRoundCount() {
         roundCount++;
     }
 
-    public void resetRoundCount() {
+    private void resetRoundCount() {
         roundCount = 0;
     }
 
     public int getRoundCount() {
         return roundCount;
+    }
+
+    public boolean isOver() {
+        return !isThreeStrike();
+    }
+
+    private boolean isThreeStrike() {
+        return countStrike(uesrNumbers, systemNumbers) == 3;
+    }
+
+    public int resultBall() {
+        return countBall(uesrNumbers, systemNumbers);
+    }
+
+    public int resultStrike() {
+        return countStrike(uesrNumbers, systemNumbers);
     }
 }
