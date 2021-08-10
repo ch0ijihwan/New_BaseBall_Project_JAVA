4becafc63de2d4fbe792cefc9c1538459c6c1bc5
cjh8746 <cjh87467@gmail.com>
2021-08-10 18:10:17 +0900

    refactor : Set으로 관리하던 랜덤 넘버를 List형태로 반환하도록 변경

--- src/main/java/model/RandomBaseballNumbers.java
+++ src/main/java/model/RandomBaseballNumbers.java
@@ -1,21 +1,25 @@
 package model;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static util.RandomUtil.generateRandomNumber;
 
 public class RandomBaseballNumbers {
     private static final int RANDOM_NUMBER_SIZE = 3;
-    private final Set<Number> threeNumbers = new LinkedHashSet<>();
+    private final Set<String> threeNumbers = new LinkedHashSet<>();
+    private final List<Number> randomNumbers;
 
     public RandomBaseballNumbers() {
         while (threeNumbers.size() < RANDOM_NUMBER_SIZE) {
-            threeNumbers.add(new Number(String.valueOf(generateRandomNumber())));
+            threeNumbers.add(String.valueOf(generateRandomNumber()));
         }
+        randomNumbers = threeNumbers.stream().map(Number::new).collect(Collectors.toList());
     }
 
-    public Set<Number> getRandomBaseballNumbers() {
-        return this.threeNumbers;
+    public List<Number> getRandomBaseballNumbers() {
+        return randomNumbers;
     }
 }
