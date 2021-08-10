9273461c3abc6385f657a9e6593efe73a74f53a9
cjh8746 <cjh87467@gmail.com>
2021-08-10 18:05:55 +0900

    refactor : countStrike 에서 Set을 받던 것을 List받도록 변경

--- src/main/java/model/score/Strike.java
+++ src/main/java/model/score/Strike.java
@@ -2,7 +2,7 @@ package model.score;
 
 import model.Number;
 
-import java.util.Set;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Strike {
@@ -13,7 +13,7 @@ public class Strike {
     private static final int NUMBER_LENGTH = 3;
     private static final int START = 0;
 
-    public static int countStrike(Set<Number> inputNumbers, Set<Number> randomNumbers) {
+    public static int countStrike(List<Number> inputNumbers, List<Number> randomNumbers) {
         return (int) IntStream.range(START, NUMBER_LENGTH)
                 .filter(index -> inputNumbers.toArray()[index].equals(randomNumbers.toArray()[index])).count();
     }
