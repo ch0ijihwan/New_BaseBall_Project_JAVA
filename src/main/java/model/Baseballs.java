1d7de0b5a0838febba9c84b8bd0ba989f1998cc9
cjh8746 <cjh87467@gmail.com>
2021-09-18 22:17:06 +0900

    refactor : 불필요한 빌드 제거 및 줄 띄움 수정

--- src/main/java/model/Baseballs.java
+++ src/main/java/model/Baseballs.java
@@ -7,17 +7,17 @@ public class Baseballs {
     private static final int STRIKE = 1;
     private static final int BALL = 2;
     private static final int NOTHING = 0;
-    private static final int NUMBER_OF_BASEBALLS = 3;
 
     private final List<Baseball> threeBalls;
-    private int strikeCount;
-    private int ballCount;
 
     public Baseballs(List<Baseball> threeBalls) {
         this.threeBalls = threeBalls;
     }
-    private void compareThreeBall(Baseballs another) {
-        return threeBalls.stream().map(another::compareBall).collect(Collectors.toList());
+
+    private List<Integer> compareThreeBall(Baseballs another) {
+        return threeBalls.stream()
+                .map(another::compareBall)
+                .collect(Collectors.toList());
     }
 
     private Integer compareBall(Baseball anotherBalls) {
@@ -27,5 +27,4 @@ public class Baseballs {
                 .findAny()
                 .orElse(NOTHING);
     }
-
 }
