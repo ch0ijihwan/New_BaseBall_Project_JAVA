34bee3d24d43da9420bde7c6823c9f3e7eef8cac
cjh8746 <cjh87467@gmail.com>
2021-09-24 15:46:51 +0900

    refactor : 변수 선언 순서 변경

--- src/main/java/model/Baseball.java
+++ src/main/java/model/Baseball.java
@@ -1,12 +1,12 @@
 package model;
 
 public class Baseball {
-    private final BaseballNumber number;
-    private final int position;
-
     private static final int STRIKE = 1;
     private static final int BALL = 2;
     private static final int NOTHING = 0;
+
+    private final BaseballNumber number;
+    private final int position;
 
     public Baseball(BaseballNumber baseBallNumber, int position) {
         number = baseBallNumber;
