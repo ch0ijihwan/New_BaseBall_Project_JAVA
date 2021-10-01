c4b6d3330f2b33934f5fa288726c0be47e80dda0
cjh8746 <cjh87467@gmail.com>
2021-10-01 22:57:17 +0900

    test :  볼 세개짜리 리스트 두개를 비교하여 볼, 스트라이크, 낫싱을 확인하여 리스트로 반환하는지 확인하는 테스트 코드 작성

--- src/test/java/model/BaseballsTest.java
+++ src/test/java/model/BaseballsTest.java
@@ -2,6 +2,7 @@ package model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,20 +19,40 @@ class BaseballsTest {
     private static final int BALL = 2;
     private static final int NOTHING = 0;
 
+
+    private Baseballs baseballs;
+    private Baseballs otherBaseballs;
+    List<Baseball> threeBalls = new ArrayList<>();
+    List<Baseball> otherThreeBalls = new ArrayList<>();
+
+
     @BeforeEach
     void setUp() {
-        List<Baseball> threeBalls = new ArrayList<>();
-
         Baseball firstBall = new Baseball(new BaseballNumber("1"), 1);
         Baseball secondBall = new Baseball(new BaseballNumber("2"), 2);
         Baseball thirdBall = new Baseball(new BaseballNumber("3"), 3);
+
+        Baseball otherFirstBall = new Baseball(new BaseballNumber("1"), 1);
+        Baseball otherSecondBall = new Baseball(new BaseballNumber("3"), 2);
+        Baseball otherThirdBall = new Baseball(new BaseballNumber("8"), 3);
 
         threeBalls.add(firstBall);
         threeBalls.add(secondBall);
         threeBalls.add(thirdBall);
 
-        Baseballs baseballs = new Baseballs(threeBalls);
+        otherThreeBalls.add(otherFirstBall);
+        otherThreeBalls.add(otherSecondBall);
+        otherThreeBalls.add(otherThirdBall);
+
     }
+
+    @Test
+    @DisplayName("공을 세개씩 가진 두 야구공을 비교하여, 그에대한 볼, 스트라이크, 낫씽을 리스트로 반환하는지 확인")
+    void compareThreeBall() {
+        baseballs = new Baseballs(threeBalls);
+        otherBaseballs = new Baseballs(otherThreeBalls);
+    }
+
 
     @ParameterizedTest
     @MethodSource("baseballParameterProvider")
@@ -42,14 +63,12 @@ class BaseballsTest {
 
     static Stream<Arguments> baseballParameterProvider() {
         return Stream.of(arguments(new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("1"), 1), STRIKE),
-                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 1),STRIKE),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("1"), 2),STRIKE),
-                arguments(new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("1"), 2),BALL),
-                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 3),BALL),
-                arguments(new Baseball(new BaseballNumber("6"), 1), new Baseball(new BaseballNumber("6"), 2),BALL),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 1),NOTHING),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 2),NOTHING));
+                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 1), STRIKE),
+                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("1"), 2), STRIKE),
+                arguments(new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("1"), 2), BALL),
+                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 3), BALL),
+                arguments(new Baseball(new BaseballNumber("6"), 1), new Baseball(new BaseballNumber("6"), 2), BALL),
+                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 1), NOTHING),
+                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 2), NOTHING));
     }
-
-
 }
