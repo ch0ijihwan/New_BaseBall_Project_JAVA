b62319a9fe778790c8c2bd552181d34bf0c78543
cjh8746 <cjh87467@gmail.com>
2021-10-04 15:52:03 +0900

    feat : 총 결과를 나타내주는 TotalResult 구현

---
+++ src/main/java/model/TotalResult.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class TotalResult {
+
+    List<BallStatus> comparedResult;
+
+    public TotalResult(List<BallStatus> comparedResult) {
+        this.comparedResult = comparedResult;
+    }
+
+    public int countBallScore() {
+        return (int) this.comparedResult.stream()
+                .filter(baseballResult -> baseballResult == BallStatus.BALL_STATUS)
+                .count();
+    }
+
+    public int countStrike() {
+        return (int) this.comparedResult.stream()
+                .filter(baseballResult -> baseballResult == BallStatus.STRIKE_STATUS)
+                .count();
+    }
+}
