aef9fd271d00c49d34bf4b6c241aed3282589651
cjh8746 <cjh87467@gmail.com>
2021-10-04 15:21:28 +0900

    feat : GameOneRound의 생성자 변경에 따른, 테스트 입력 값 변경

--- src/test/java/model/GameOneRoundTest.java
+++ src/test/java/model/GameOneRoundTest.java
@@ -2,7 +2,6 @@ package model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,39 +20,33 @@ class GameOneRoundTest {
 
     @BeforeEach
     void setUp() {
-        gameOneRound = new GameOneRound(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)),
+        gameOneRound = new GameOneRound(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
                 Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)));
     }
 
     @ParameterizedTest
     @MethodSource("threeStrikeCountParameterProvider")
     @DisplayName("threeStrike 인 경우 true 반환을 확인.")
-    void isOver(List<Baseball> inputNumbers, List<Baseball> randomNumbers, boolean expect) {
+    void isOver(Baseballs inputNumbers, List<Baseball> randomNumbers, boolean expect) {
         GameOneRound gameOneRound = new GameOneRound(inputNumbers, randomNumbers);
         boolean resultStrikeCount = gameOneRound.isThreeStrike();
         assertThat(resultStrikeCount).isEqualTo(expect);
     }
 
     static Stream<Arguments> threeStrikeCountParameterProvider() {
-        return Stream.of(arguments(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)),
+        return Stream.of(arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
                         Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)), true),
 
-                arguments(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)),
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
                         Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0)), false),
 
-                arguments(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)),
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
                         Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2)), false),
 
-                arguments(Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0)),
+                arguments(new Baseballs( Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0))),
                         Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0)), true),
 
-                arguments(Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2)),
+                arguments(new Baseballs( Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2))),
                         Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2)), true));
-    }
-
-    @Test
-    @DisplayName("객체 생성시 라운드 카운트를 1로 초기화 하는지 확인")
-    void checkResetRoundCount() {
-        assertThat(gameOneRound.getRoundCount()).isEqualTo(START_ROUND_COUNT);
     }
 }
