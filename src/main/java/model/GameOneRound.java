3043ccc11b5dcab9388032d06763851d7f1660c7
cjh8746 <cjh87467@gmail.com>
2021-10-06 15:41:06 +0900

    refactor : MVC 패던에 맞춰서, 컨트롤러 및  GameOneRound을 수정하여, GameOneRound 모델에서 값을 바로 뷰로 보내던 문제를 해결

--- src/main/java/model/GameOneRound.java
+++ src/main/java/model/GameOneRound.java
@@ -1,34 +1,27 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static controller.BaseballController.resultBallCountAndStrikeCount;
-import static view.Input.inputBaseballNumbers;
-
 public class GameOneRound {
-    private final Baseballs systemBaseballs;
     private TotalResult totalResult;
     private List<BallStatus> comparedResult;
+    private List<Integer> result = new ArrayList<>();
 
-
-    public GameOneRound(Baseballs uesrBaseballNumbers, List<Baseball> systemBaseballNumbers) {
-        systemBaseballs = new Baseballs(systemBaseballNumbers);
-        comparedResult = uesrBaseballNumbers.compareThreeBall(systemBaseballs);
+    public GameOneRound(Baseballs uesrBaseballNumbers, Baseballs systemBaseballNumbers) {
+        comparedResult = uesrBaseballNumbers.compareThreeBall(systemBaseballNumbers);
     }
 
-    public void operate() {
+    public List<Integer> operate() {
         totalResult = new TotalResult(this.comparedResult);
-        resultBallCountAndStrikeCount(totalResult.countBallScore(),totalResult.countStrike());
-        Baseballs inputNewBaseballs = enterNumbers();
-        comparedResult = inputNewBaseballs.compareThreeBall(systemBaseballs);
+        result.add(totalResult.countBallScore());
+        result.add(totalResult.countStrikeScore());
+
+        return result;
     }
 
     public boolean isThreeStrike() {
         totalResult = new TotalResult(comparedResult);
         return totalResult.isResultThreeStrike();
-    }
-
-    private Baseballs enterNumbers() {
-        return new Baseballs(inputBaseballNumbers());
     }
 }
