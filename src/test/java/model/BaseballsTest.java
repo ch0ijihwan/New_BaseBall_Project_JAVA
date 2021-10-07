2677f9854882c759112f60fcd1dfe790e87ac39d
cjh8746 <cjh87467@gmail.com>
2021-10-08 00:51:38 +0900

    test : 새로운 숫자야구 넘버를 입력 받을 시 threeBalls 리스트를 생성하는지에 대한 테스트 코드

--- src/test/java/model/BaseballsTest.java
+++ src/test/java/model/BaseballsTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BaseballsTest {
@@ -38,6 +40,32 @@ class BaseballsTest {
         otherThreeBalls.add(otherSecondBall);
         otherThreeBalls.add(otherThirdBall);
     }
+
+    @ParameterizedTest
+    @MethodSource("createBaseballsParameterProvider")
+    @DisplayName("새로운 숫자야구 넘버를 입력 받을 시 threeBalls 리스트를 생성하는지에 대한 테스트 코드")
+    void makeBaseballs(String[] inputNumbers, Baseballs expect) {
+        baseballs = new Baseballs(inputNumbers);
+        assertAll(
+                () -> assertThat(baseballs.compareThreeBall(expect).get(0)).isEqualTo(BallStatus.STRIKE_STATUS),
+                () -> assertThat(baseballs.compareThreeBall(expect).get(1)).isEqualTo(BallStatus.STRIKE_STATUS),
+                () -> assertThat(baseballs.compareThreeBall(expect).get(2)).isEqualTo(BallStatus.STRIKE_STATUS)
+        );
+    }
+
+    static Stream<Arguments> createBaseballsParameterProvider() {
+        return Stream.of(
+                arguments((Object) new String[]{"1", "2", "3"},
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)))),
+
+                arguments((Object) new String[]{"5", "4", "8"},
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("5"), 0), new Baseball(new BaseballNumber("4"), 1), new Baseball(new BaseballNumber("8"), 2)))),
+
+                arguments((Object) new String[]{"9", "2", "5"},
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("9"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("5"), 2))))
+        );
+    }
+
 
     @Test
     @DisplayName("공을 세개씩 가진 두 야구공을 비교하여, 그에대한 볼, 스트라이크, 낫씽을 리스트로 반환하는지 확인")
@@ -69,4 +97,6 @@ class BaseballsTest {
                 arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 1), BallStatus.NOTHING_STATUS),
                 arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 2), BallStatus.NOTHING_STATUS));
     }
+
+
 }
