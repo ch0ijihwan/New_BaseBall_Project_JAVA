7ad2827a1ecc2c9dfb09216e7eddb95573b4693f
cjh8746 <cjh87467@gmail.com>
2021-10-08 00:20:05 +0900

    test : 볼과 스트라이크의 개수를 제대로 반환하는지 확인하는 테스트 코드

--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -9,9 +9,26 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class RoundTest {
+    private static final int BALL_COUNT_INDEX = 0;
+    public static final int STRIKE_COUNT_INDEX = 1;
+    private Round round;
+
+    @ParameterizedTest
+    @MethodSource("judgeResultParameterProvider")
+    @DisplayName(" TotalResult를 통해 스트라이크 개수와 볼 개수를 받아 반환하는지 확인")
+    void judgeResult(Baseballs inputNumbers, Baseballs randomNumbers, int ballCount, int strikeCount) {
+        round = new Round(inputNumbers, randomNumbers);
+
+        assertAll(
+                () -> assertThat(round.judgeResult().get(BALL_COUNT_INDEX)).isEqualTo(ballCount),
+                () -> assertThat(round.judgeResult().get(STRIKE_COUNT_INDEX)).isEqualTo(strikeCount)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("threeStrikeCountParameterProvider")
     @DisplayName("threeStrike 인 경우 true 반환을 확인.")
@@ -19,6 +36,23 @@ class RoundTest {
         Round round = new Round(inputNumbers, randomNumbers);
         boolean resultStrikeCount = round.isThreeStrike();
         assertThat(resultStrikeCount).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> judgeResultParameterProvider() {
+        return Stream.of(arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
+                        (new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)))), 0, 3),
+
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("3"), 0), new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("2"), 2))), 3, 0),
+
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("9"), 1), new Baseball(new BaseballNumber("8"), 2))), 0, 1),
+
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("2"), 0), new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("8"), 2))), 2, 0),
+
+                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("5"), 2))),
+                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("4"), 2))), 0, 2));
     }
 
     static Stream<Arguments> threeStrikeCountParameterProvider() {
