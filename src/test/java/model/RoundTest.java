106242b29c66f833d6515b6b20c77bd5cdc347bb
cjh8746 <cjh87467@gmail.com>
2021-10-11 14:36:44 +0900

    test : 테스트 코드 수정
    메소드 소드를 Csv소스로 변경.
    
    테스트시 기본 baseballs와 입력받은 otherBaseballs를 비교하도록 변경

--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -2,73 +2,54 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class RoundTest {
-    private static final int BALL_COUNT_INDEX = 0;
-    public static final int STRIKE_COUNT_INDEX = 1;
     private Round round;
+    private Baseballs baseballs;
+    private Baseballs otherBaseballs;
 
     @ParameterizedTest
-    @MethodSource("judgeResultParameterProvider")
-    @DisplayName(" TotalResult를 통해 스트라이크 개수와 볼 개수를 받아 반환하는지 확인")
-    void judgeResult(Baseballs inputNumbers, Baseballs randomNumbers, int ballCount, int strikeCount) {
-        round = new Round(inputNumbers, randomNumbers);
+    @CsvSource(value = {"1,2,3,0,3", "3,1,2,3,0", "3,2,1,2,1", "4,5,6,0,0"})
 
+    @DisplayName("  스트라이크 개수와 볼 개수를 받아 반환하는지 확인")
+    void judgeResult(String firstNumber, String secondNumber, String thirdNumber, int ballExpet, int strikeExpet) {
+        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
+        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
+        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
+        Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
+        Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
+        Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
+
+        baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
+        otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
+        round = new Round(baseballs, otherBaseballs);
         assertAll(
-                () -> assertThat(round.judgeResult().get(BALL_COUNT_INDEX)).isEqualTo(ballCount),
-                () -> assertThat(round.judgeResult().get(STRIKE_COUNT_INDEX)).isEqualTo(strikeCount)
+                () -> assertThat(round.getBallCount()).isEqualTo(ballExpet),
+                () -> assertThat(round.getStrikeCount()).isEqualTo(strikeExpet)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("threeStrikeCountParameterProvider")
+    @CsvSource(value = {"1,2,3,true", "3,2,1,false"})
     @DisplayName("threeStrike 인 경우 true 반환을 확인.")
-    void isOver(Baseballs inputNumbers, Baseballs randomNumbers, boolean expect) {
-        Round round = new Round(inputNumbers, randomNumbers);
-        boolean resultStrikeCount = round.isThreeStrike();
-        assertThat(resultStrikeCount).isEqualTo(expect);
-    }
+    void isOver(String firstNumber, String secondNumber, String thirdNumber, boolean expect) {
+        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
+        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
+        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
+        Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
+        Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
+        Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
 
-    static Stream<Arguments> judgeResultParameterProvider() {
-        return Stream.of(arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        (new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)))), 0, 3),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("3"), 0), new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("2"), 2))), 3, 0),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("9"), 1), new Baseball(new BaseballNumber("8"), 2))), 0, 1),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("2"), 0), new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("8"), 2))), 2, 0),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("5"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("4"), 2))), 0, 2));
-    }
-
-    static Stream<Arguments> threeStrikeCountParameterProvider() {
-        return Stream.of(arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        (new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)))), true),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        (new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0)))), false),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2))), false),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 0))), true),
-
-                arguments(new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2))),
-                        new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("4"), 0), new Baseball(new BaseballNumber("5"), 1), new Baseball(new BaseballNumber("6"), 2))), true));
+        baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
+        otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
+        Round round = new Round(baseballs, otherBaseballs);
+       
+        assertThat(round.isThreeStrike()).isEqualTo(expect);
     }
 }
