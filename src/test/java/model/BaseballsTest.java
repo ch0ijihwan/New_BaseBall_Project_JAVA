e63536ecfa9fa97f938179f7a3f5e155dbc7804b
cjh8746 <cjh87467@gmail.com>
2021-10-11 13:58:43 +0900

    test : 테스트 코드 수정
    한번 밖에 동작하지 않았던, 셋업 삭제.
    add로 일일히 요소를 추가해주더문제를, asList를 써서 한번에 추가해
    
    Baseball 객체에 대한 테스트 부분 삭제

--- src/test/java/model/BaseballsTest.java
+++ src/test/java/model/BaseballsTest.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,7 +7,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -20,30 +18,10 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BaseballsTest {
     private Baseballs baseballs;
-    List<Baseball> threeBalls = new ArrayList<>();
-    List<Baseball> otherThreeBalls = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
-        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
-        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
-        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
-
-        Baseball otherFirstBall = new Baseball(new BaseballNumber("1"), 0);
-        Baseball otherSecondBall = new Baseball(new BaseballNumber("3"), 1);
-        Baseball otherThirdBall = new Baseball(new BaseballNumber("8"), 2);
-
-        threeBalls.add(firstBall);
-        threeBalls.add(secondBall);
-        threeBalls.add(thirdBall);
-
-        otherThreeBalls.add(otherFirstBall);
-        otherThreeBalls.add(otherSecondBall);
-        otherThreeBalls.add(otherThirdBall);
-    }
+    private Baseballs otherBaseballs;
 
     @ParameterizedTest
-    @CsvSource(value = {"1235", "4512", "1234", "7658", "3758"})
+    @CsvSource(value = {"12", "1234"})
     @DisplayName("생성자에서 새로운 숫자야구 넘버를 받을 때, 올바르지 않은 값을 받은경우 (입력값이 세자리가 아닌경우) 예외 처리를 하는지 확인")
     void validateNumber(String value) {
         String[] tokens = value.split("");
@@ -53,7 +31,7 @@ class BaseballsTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"133", "454", "121", "155", "338", "992"})
+    @CsvSource(value = {"133", "999"})
     @DisplayName("생성자에서 새로운 숫자야구 넘버를 받을 때, 올바르지 않은 값을 받은경우 (중복된 수가 있는 경우) 예외 처리를 하는지 확인")
     void validateNumber2(String value) {
         String[] tokens = value.split("");
@@ -78,43 +56,28 @@ class BaseballsTest {
         return Stream.of(
                 arguments(new String[]{"1", "2", "3"},
                         new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("1"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("3"), 2)))),
-
                 arguments(new String[]{"5", "4", "8"},
                         new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("5"), 0), new Baseball(new BaseballNumber("4"), 1), new Baseball(new BaseballNumber("8"), 2)))),
-
                 arguments(new String[]{"9", "2", "5"},
                         new Baseballs(Arrays.asList(new Baseball(new BaseballNumber("9"), 0), new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("5"), 2))))
         );
     }
 
     @Test
-    @DisplayName("공을 세개씩 가진 두 야구공을 비교하여, 그에대한 볼, 스트라이크, 낫씽을 리스트로 반환하는지 확인")
+    @DisplayName("공을 세개씩 가진 두 야구공리스트을 비교하여, 그에대한 볼, 스트라이크, 낫씽을 리스트로 반환하는지 확인")
     void compareThreeBall() {
-        baseballs = new Baseballs(threeBalls);
-        Baseballs otherBaseballs = new Baseballs(otherThreeBalls);
-        List<BallStatus> expect = new ArrayList<>();
-        expect.add(BallStatus.STRIKE_STATUS);
-        expect.add(BallStatus.NOTHING_STATUS);
-        expect.add(BallStatus.BALL_STATUS);
+
+        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
+        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
+        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
+        Baseball otherFirstBall = new Baseball(new BaseballNumber("1"), 0);
+        Baseball otherSecondBall = new Baseball(new BaseballNumber("3"), 1);
+        Baseball otherThirdBall = new Baseball(new BaseballNumber("8"), 2);
+
+        baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
+        otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
+        List<BallStatus> expect = Arrays.asList(BallStatus.STRIKE_STATUS, BallStatus.NOTHING_STATUS, BallStatus.BALL_STATUS);
 
         assertThat(baseballs.compareThreeBall(otherBaseballs)).isEqualTo(expect);
-    }
-
-    @ParameterizedTest
-    @MethodSource("baseballParameterProvider")
-    @DisplayName("서로 다른 공 두개를 서로 비교하여 볼 스트라이크를 제대로 반환하는지 확인하는 테스트 코드")
-    void compareBallOneByOne(Baseball baseball, Baseball anoterBaseball, BallStatus expect) {
-        assertThat(baseball.checkBaseball(anoterBaseball)).isEqualTo(expect);
-    }
-
-    static Stream<Arguments> baseballParameterProvider() {
-        return Stream.of(arguments(new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("1"), 1), BallStatus.STRIKE_STATUS),
-                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 1), BallStatus.STRIKE_STATUS),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("1"), 2), BallStatus.STRIKE_STATUS),
-                arguments(new Baseball(new BaseballNumber("1"), 1), new Baseball(new BaseballNumber("1"), 2), BallStatus.BALL_STATUS),
-                arguments(new Baseball(new BaseballNumber("2"), 1), new Baseball(new BaseballNumber("2"), 3), BallStatus.BALL_STATUS),
-                arguments(new Baseball(new BaseballNumber("6"), 1), new Baseball(new BaseballNumber("6"), 2), BallStatus.BALL_STATUS),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 1), BallStatus.NOTHING_STATUS),
-                arguments(new Baseball(new BaseballNumber("1"), 2), new Baseball(new BaseballNumber("3"), 2), BallStatus.NOTHING_STATUS));
     }
 }
