e98d4de90f00b7d6605312c01f65f4695c646982
cjh8746 <cjh87467@gmail.com>
2021-10-12 14:52:50 +0900

    refactor : 메서드 이름 변경

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -2,31 +2,33 @@ package model;
 
 import java.util.List;
 
-public class Round {
+public class Game {
     private static final int THREE_STRIKE = 3;
     private Baseballs systemBaseballs;
     private List<BallStatus> comparedResult;
 
-    public Round(Baseballs userBaseballs, Baseballs systemBaseballs) {
-        this.systemBaseballs =  systemBaseballs;
+    public Game(Baseballs userBaseballs, Baseballs systemBaseballs) {
+        this.systemBaseballs = systemBaseballs;
         comparedResult = systemBaseballs.compareThreeBall(userBaseballs);
     }
 
     public boolean isThreeStrike() {
-        return getStrikeCount() == THREE_STRIKE;
+        return contStrikeStatus() == THREE_STRIKE;
     }
-    public void InputNewBaseballs(Baseballs userBaseballs){
+
+    public void inputNewBaseballs(Baseballs userBaseballs) {
         comparedResult = systemBaseballs.compareThreeBall(userBaseballs);
     }
-    public int getBallCount() {
-        return countScore(BallStatus.BALL_STATUS);
+
+    public int countBallStatus() {
+        return countStatus(BallStatus.BALL);
     }
 
     public int getStrikeCount(){
         return countScore(BallStatus.STRIKE_STATUS);
     }
 
-    private int countScore(BallStatus desiredValue) {
+    private int countStatus(BallStatus desiredValue) {
         return (int) this.comparedResult.stream()
                 .filter(baseballResult -> baseballResult == desiredValue)
                 .count();
