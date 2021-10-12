5047d7ba0752c9aa2580807d5bdf7e4a80def3f8
cjh8746 <cjh87467@gmail.com>
2021-10-12 16:01:51 +0900

    test : 생성자로 부터 String 값이 아닌 int 형 값을 받게 함에 따른 테스트 코드 수정. 및 예외처리 범위 수정

--- src/test/java/model/BaseballNumberTest.java
+++ src/test/java/model/BaseballNumberTest.java
@@ -3,7 +3,6 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,21 +13,12 @@ class BaseballNumberTest {
     private BaseballNumber anotherNumber;
 
     @ParameterizedTest
-    @ValueSource(strings = {"=", "+", "%", "a", "?", "0"})
-    @DisplayName("받은 값이 1 ~ 9 숫자가 아닌 경우 예외처리를 반환")
-    void isNumber(String value) {
+    @ValueSource(strings = {"0", "-1"})
+    @DisplayName("생성자로부터 받은 받은 값이 1 ~ 9 사이에 있는 숫자가 아닌 경우 예외처리를 반환")
+    void isNumber(int value) {
         assertThatThrownBy(() -> new BaseballNumber(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1~9에 있는 숫자가 아닙니다.");
-    }
-
-    @ParameterizedTest
-    @EmptySource
-    @DisplayName("아무 값도 입력되지 않았을 경우 예외처리 반환")
-    void isBlank(String empty) {
-        assertThatThrownBy(() -> new BaseballNumber(empty))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("아무 값도 입력되지 않았습니다.");
     }
 
     @Test
