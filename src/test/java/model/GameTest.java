bfb92c0fefc06f330540d9f88794e00e3f744f44
cjh8746 <cjh87467@gmail.com>
2021-10-13 15:37:45 +0900

    test : 중복되는 코드를 setUp으로 빼줌

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,47 +10,50 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-class RoundTest {
-    private Round round;
+class GameTest {
+    private Game game;
     private Baseballs baseballs;
     private Baseballs otherBaseballs;
+    private Baseball firstBall;
+    private Baseball secondBall;
+    private Baseball thirdBall;
+
+    @BeforeEach
+    void setUp() {
+        firstBall = new Baseball(new BaseballNumber(1), 0);
+        secondBall = new Baseball(new BaseballNumber(2), 1);
+        thirdBall = new Baseball(new BaseballNumber(3), 2);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"1,2,3,0,3", "3,1,2,3,0", "3,2,1,2,1", "4,5,6,0,0"})
-
     @DisplayName("  스트라이크 개수와 볼 개수를 받아 반환하는지 확인")
-    void judgeResult(String firstNumber, String secondNumber, String thirdNumber, int ballExpet, int strikeExpet) {
-        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
-        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
-        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
+    void judgeResult(int firstNumber, int secondNumber, int thirdNumber, int ballExpet, int strikeExpet) {
         Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
         Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
         Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
 
         baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
         otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
-        round = new Round(baseballs, otherBaseballs);
+        game = new Game(baseballs, otherBaseballs);
         assertAll(
-                () -> assertThat(round.getBallCount()).isEqualTo(ballExpet),
-                () -> assertThat(round.getStrikeCount()).isEqualTo(strikeExpet)
+                () -> assertThat(game.countBallStatus()).isEqualTo(ballExpet),
+                () -> assertThat(game.contStrikeStatus()).isEqualTo(strikeExpet)
         );
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1,2,3,true", "3,2,1,false"})
     @DisplayName("threeStrike 인 경우 true 반환을 확인.")
-    void isOver(String firstNumber, String secondNumber, String thirdNumber, boolean expect) {
-        Baseball firstBall = new Baseball(new BaseballNumber("1"), 0);
-        Baseball secondBall = new Baseball(new BaseballNumber("2"), 1);
-        Baseball thirdBall = new Baseball(new BaseballNumber("3"), 2);
+    void isOver(int firstNumber, int secondNumber, int thirdNumber, boolean expect) {
         Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
         Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
         Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
 
         baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
         otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
-        Round round = new Round(baseballs, otherBaseballs);
-       
-        assertThat(round.isThreeStrike()).isEqualTo(expect);
+        Game game = new Game(baseballs, otherBaseballs);
+
+        assertThat(game.isThreeStrike()).isEqualTo(expect);
     }
 }
