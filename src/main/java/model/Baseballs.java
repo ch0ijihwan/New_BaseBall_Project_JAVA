8c86bf7269ad043a63144860d806f5c94032389b
cjh8746 <cjh87467@gmail.com>
2021-10-13 15:34:49 +0900

    refactor : DTO에 대한 validateNumber 제거
    입력받은 문자에 대한 예외처리를 DTO에 넣어줌

--- src/main/java/model/Baseballs.java
+++ src/main/java/model/Baseballs.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,22 +15,12 @@ public class Baseballs {
         this.threeBalls = threeBalls;
     }
 
-    public Baseballs(String[] inputNumbers) {
-        threeBalls  = new ArrayList<>();
-        validateNumber(inputNumbers);
-
+    public Baseballs(int[] inputNumbers) {
+        threeBalls = new ArrayList<>();
         IntStream.range(0, NUMBER_LENGTH)
-                .forEach(position -> threeBalls.add(new Baseball(new BaseballNumber(inputNumbers[position]), position)));
+                .forEach(index -> threeBalls.add(new Baseball(inputNumbers[index], index)));
     }
 
-    private void validateNumber(String[] token) {
-        if (token.length != NUMBER_LENGTH) {
-            throw new IllegalArgumentException("입력값이 3자리가 아닙니다.");
-        }
-        if (Arrays.stream(token).distinct().count() != 3) {
-            throw new IllegalArgumentException("중복되는 수가 있습니다.");
-        }
-    }
 
     public List<BallStatus> compareThreeBall(Baseballs another) {
         return threeBalls.stream()
@@ -42,8 +31,8 @@ public class Baseballs {
     private BallStatus compareBall(Baseball anotherBall) {
         return threeBalls.stream()
                 .map(anotherBall::checkBaseball)
-                .filter(ballStatus -> ballStatus == BallStatus.BALL_STATUS || ballStatus == BallStatus.STRIKE_STATUS)
+                .filter(ballStatus -> ballStatus == BallStatus.BALL || ballStatus == BallStatus.STRIKE)
                 .findAny()
-                .orElse(BallStatus.NOTHING_STATUS);
+                .orElse(BallStatus.NOTHING);
     }
 }
