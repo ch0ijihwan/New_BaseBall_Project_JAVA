c0736f1c86608327dc016e00a67d4263a0d3b67f
cjh8746 <cjh87467@gmail.com>
2021-10-13 15:35:21 +0900

    refactor : 불필요한 줄 띄움 삭제

--- src/main/java/model/DtoInputNumbers.java
+++ src/main/java/model/DtoInputNumbers.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class DtoInputNumbers {
     private static final int NUMBER_LENGTH = 3;
     private int[] inputNumbers;
-    public DtoInputNumbers(String [] tokens){
+
+    public DtoInputNumbers(String[] tokens) {
         this.inputNumbers = Arrays.stream(tokens).mapToInt(Integer::parseInt).toArray();
         validateNumbers(inputNumbers);
     }
-    public int[] getInputNumbers(){
+
+    public int[] getInputNumbers() {
         return inputNumbers;
     }
-
 
     private void validateNumbers(int[] inputValue) {
         if (inputValue.length != NUMBER_LENGTH) {
