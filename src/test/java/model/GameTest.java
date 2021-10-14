b199d13875cce55a2e935c4dea0f4ab71b3b289b
cjh8746 <cjh87467@gmail.com>
2021-10-14 22:32:14 +0900

    test :  테스트 코드 분리. countBall 메서드와 countStrike 메서드를 테스트 하는 테스트 코드 분리

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -20,25 +20,40 @@ class GameTest {
 
     @BeforeEach
     void setUp() {
-        firstBall = new Baseball(new BaseballNumber(1), 0);
-        secondBall = new Baseball(new BaseballNumber(2), 1);
-        thirdBall = new Baseball(new BaseballNumber(3), 2);
+        firstBall = new Baseball(1, 0);
+        secondBall = new Baseball(2, 1);
+        thirdBall = new Baseball(3, 2);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3,0,3", "3,1,2,3,0", "3,2,1,2,1", "4,5,6,0,0"})
-    @DisplayName("  스트라이크 개수와 볼 개수를 받아 반환하는지 확인")
-    void judgeResult(int firstNumber, int secondNumber, int thirdNumber, int ballExpet, int strikeExpet) {
-        Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
-        Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
-        Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
+    @CsvSource(value = {"1,2,3,0", "3,1,2,3", "3,2,1,2", "4,5,6,0"})
+    @DisplayName("countBall 실행시 볼 개수를 받아 반환하는지 확인")
+    void countBall(int firstNumber, int secondNumber, int thirdNumber, int ballExpect) {
+        Baseball otherFirstBall = new Baseball(firstNumber, 0);
+        Baseball otherSecondBall = new Baseball(secondNumber, 1);
+        Baseball otherThirdBall = new Baseball(thirdNumber, 2);
 
         baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
         otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
         game = new Game(baseballs, otherBaseballs);
         assertAll(
-                () -> assertThat(game.countBallStatus()).isEqualTo(ballExpet),
-                () -> assertThat(game.contStrikeStatus()).isEqualTo(strikeExpet)
+                () -> assertThat(game.countBallStatus()).isEqualTo(ballExpect)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,3", "3,1,2,0", "3,2,1,1", "4,5,6,0"})
+    @DisplayName("countStrike 실행시 스트라이크 개수를 받아 반환하는지 확인")
+    void countStrike(int firstNumber, int secondNumber, int thirdNumber, int strikeExpect) {
+        Baseball otherFirstBall = new Baseball(firstNumber, 0);
+        Baseball otherSecondBall = new Baseball(secondNumber, 1);
+        Baseball otherThirdBall = new Baseball(thirdNumber, 2);
+
+        baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
+        otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
+        game = new Game(baseballs, otherBaseballs);
+        assertAll(
+                () -> assertThat(game.contStrikeStatus()).isEqualTo(strikeExpect)
         );
     }
 
@@ -46,9 +61,9 @@ class GameTest {
     @CsvSource(value = {"1,2,3,true", "3,2,1,false"})
     @DisplayName("threeStrike 인 경우 true 반환을 확인.")
     void isOver(int firstNumber, int secondNumber, int thirdNumber, boolean expect) {
-        Baseball otherFirstBall = new Baseball(new BaseballNumber(firstNumber), 0);
-        Baseball otherSecondBall = new Baseball(new BaseballNumber(secondNumber), 1);
-        Baseball otherThirdBall = new Baseball(new BaseballNumber(thirdNumber), 2);
+        Baseball otherFirstBall = new Baseball(firstNumber, 0);
+        Baseball otherSecondBall = new Baseball(secondNumber, 1);
+        Baseball otherThirdBall = new Baseball(thirdNumber, 2);
 
         baseballs = new Baseballs(Arrays.asList(firstBall, secondBall, thirdBall));
         otherBaseballs = new Baseballs(Arrays.asList(otherFirstBall, otherSecondBall, otherThirdBall));
