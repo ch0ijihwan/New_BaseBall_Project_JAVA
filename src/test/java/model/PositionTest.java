9adafc718cbac4e66cb043b49ebdfe40d276b7b5
cjh8746 <cjh87467@gmail.com>
2021-10-14 16:25:34 +0900

    test : value() 메서드에 대한 테스트 코드 추가.

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -1,9 +1,11 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
@@ -17,6 +19,21 @@ class PositionTest {
         assertThatThrownBy(() -> new Position(actual))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("포지선의 범위가 0~2 사이가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName(" value()메서드 호출시,생성자에서 부터 받은 Postion을 제대로 반환하는지 확인하는 테스트 코드.")
+    void value()
+    {
+        //given
+        int expect = 0;
+        Position position = new Position(expect);
+
+        //when
+        int actual = position.value();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
     }
 
 }
