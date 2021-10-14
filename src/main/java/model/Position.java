1a1cb72946752bbe054f1a76c12de5f5f9fc2f45
cjh8746 <cjh87467@gmail.com>
2021-10-14 22:30:50 +0900

    refactor : 이퀄즈 오버라이드 및 필드명 수정
    포지션을 비교하기 위한 equals 추가, position 필드를 파이널로 선언, 예외처리 메시지 오타 수정.

--- src/main/java/model/Position.java
+++ src/main/java/model/Position.java
@@ -1,23 +1,37 @@
 package model;
 
+import java.util.Objects;
+
 public class Position {
     private static final int MIN_BOUNDARY = 0;
     private static final int MAX_BOUNDARY = 2;
-    private int position;
+    private final int index;
 
-    public Position(int position) {
-        validateRangeOfNumber(position);
-        this.position = position;
+    public Position(int index) {
+        validateRangeOfNumber(index);
+        this.index = index;
     }
 
     public int value() {
-        return position;
+        return index;
     }
 
     private void validateRangeOfNumber(int position) {
         if (!(MIN_BOUNDARY <= position && position <= MAX_BOUNDARY)) {
-            throw new IllegalArgumentException("포지선의 범위가 0~2 사이가 아닙니다.");
+            throw new IllegalArgumentException("공위치의 인덱스 범위가 0~2 사이가 아닙니다.");
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return index == position1.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
+    }
 }
