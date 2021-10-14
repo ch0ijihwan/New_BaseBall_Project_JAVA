4ee52249f204d5e2f82a904d3903a37f93f4f6b8
cjh8746 <cjh87467@gmail.com>
2021-10-14 16:21:23 +0900

    test : 가독성을 위한 띄어쓰기 추가

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PositionTest {
     @ParameterizedTest
     @DisplayName("0 ~ 2 사이에 있지 않은 포지션을  예외처리를 반환함을 확인하는 테스트 코드.")
-    @CsvSource(value = {"-1","3"})
+    @CsvSource(value = {"-1", "3"})
     void validateRangeOfNumber(String value) {
         //given
         int actual = Integer.parseInt(value);
