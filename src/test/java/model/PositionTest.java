a634f64c9a94781662b175047a4a917909289c7e
cjh8746 <cjh87467@gmail.com>
2021-10-15 23:50:02 +0900

    test : 불필요한 과정 삭제.

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -13,11 +13,9 @@ class PositionTest {
     @ParameterizedTest
     @DisplayName("0 ~ 2 사이에 있지 않은 포지션을  예외처리를 반환함을 확인하는 테스트 코드.")
     @CsvSource(value = {"-1", "3"})
-    void validateRangeOfNumber(String value) {
-        //given
-        int actual = Integer.parseInt(value);
+    void validateRangeOfNumber(int value) {
         //then
-        assertThatThrownBy(() -> new Position(actual))
+        assertThatThrownBy(() -> new Position(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("공위치의 인덱스 범위가 0~2 사이가 아닙니다.");
     }
