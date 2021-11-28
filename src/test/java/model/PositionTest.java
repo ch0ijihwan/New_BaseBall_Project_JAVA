161d9a829d381bd6c96db2754bd49d03b6069c81
cjh8746 <cjh87467@gmail.com>
2021-11-28 20:31:34 +0900

    test : 테스트 코드 수정

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -27,7 +27,7 @@ class PositionTest {
 
     @ParameterizedTest
     @DisplayName("Position 객체 생성 시, 생성자의 파라미터로 부터 받은 값이 1,2,3 이 아니라면 예외처리를 반환한다.")
-    @CsvSource(value =  {"0","4"})
+    @CsvSource(value = {"0", "4"})
     void validatePositionBoundary(int input) {
         //then
         assertThatThrownBy(() -> new Position(input))
@@ -37,16 +37,16 @@ class PositionTest {
 
     @Test
     @DisplayName("getValue() 호출 시, Position 의 value 값을 반환한다.")
-    void getValue(){
+    void getValue() {
         //given
         int input = 1;
-        int expect =1;
+        int expect = 1;
         position = new Position(input);
 
         //when
         int actual = position.getValue();
 
         //then
-        assertThat(actual).isEqualTo(1);
+        assertThat(actual).isEqualTo(expect);
     }
 }
