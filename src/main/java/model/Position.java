aa6e91df5d7b6b9c28ae6e1f57900b61d716a6a8
cjh8746 <cjh87467@gmail.com>
2021-11-28 13:45:59 +0900

    feat : Position 구현

---
+++ src/main/java/model/Position.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.Objects;
+
+public class Position {
+    private static final int MAX_BALL_NUMBER_RANGE = 3;
+    private static final int MIN_BALL_NUMBER_RANGE = 1;
+    private final int value;
+
+    public Position(final int value) {
+        validateNumberBoundary(value);
+        this.value = value;
+    }
+
+    private void validateNumberBoundary(final int value) {
+        if (MIN_BALL_NUMBER_RANGE > value || value >= MAX_BALL_NUMBER_RANGE) {
+            throw new IllegalArgumentException("공의 Position 이 1,2,3 의 숫자가 아닙니다.");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "value=" + value +
+                '}';
+    }
+}
