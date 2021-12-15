259d243f7554076ef6a8149b69770f55efb9e4d1
cjh8746 <cjh87467@gmail.com>
2021-12-15 15:42:25 +0900

    test : 클래스 이름 변경

--- src/test/java/util/RandomUtilTest.java
+++ src/test/java/util/RandomUtilTest.java
@@ -3,13 +3,14 @@ package util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static model.util.randomUtil.generateRandomNumber;
+import static model.util.RandomUtil.generateRandomNumber;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomUtilTest {
     @Test
     @DisplayName("generateRandomNumber() 호출 시, 0보다 크고 10보다 작은 한 자리 숫자를 반환한다.")
     void greaterThanZeroAndLessThanTen() {
-        assertThat(generateRandomNumber()).isPositive().isLessThan(10);
+        assertThat(generateRandomNumber()).isPositive()
+                .isLessThan(10);
     }
 }
