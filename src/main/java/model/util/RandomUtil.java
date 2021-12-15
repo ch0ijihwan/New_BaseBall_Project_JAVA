3fa7932e116db3e227f2f10734bd59d7d670492e
cjh8746 <cjh87467@gmail.com>
2021-12-15 15:42:49 +0900

    feat : RandomUtil 구현

---
+++ src/main/java/model/util/RandomUtil.java
@@ -0,0 +1,16 @@
+package model.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+    private RandomUtil() {
+    }
+
+    private static final int RANDOM_MIN_BOUNDARY = 1;
+    private static final int RANDOM_MAX_BOUNDARY = 9;
+    private static final Random RANDOM = new Random();
+
+    public static int generateRandomNumber() {
+        return RANDOM.nextInt(RANDOM_MAX_BOUNDARY) + RANDOM_MIN_BOUNDARY;
+    }
+}
