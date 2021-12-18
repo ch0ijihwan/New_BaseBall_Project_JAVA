c81b6fc7b7646a3b4b8a84e839f6250fe3832807
cjh8746 <cjh87467@gmail.com>
2021-12-18 18:26:23 +0900

    feat : APP 생성

---
+++ src/main/java/application/APP.java
@@ -0,0 +1,9 @@
+package application;
+
+import controller.BaseballGameController;
+
+public class APP {
+    public static void main(String[] args) {
+        new BaseballGameController();
+    }
+}
